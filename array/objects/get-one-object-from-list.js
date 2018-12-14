6f72deb910d282f615b57d7300a6e115f4603a9f
Luiz Felipe <23662020+lffg@users.noreply.github.com>
2018-12-14 19:30:10 -0200

    add example

---
+++ array/objects/get-one-object-from-list.js
@@ -0,0 +1,34 @@
+// 14/12/2018
+
+const example = [{
+  name: 'Xii Jin Ping',
+  age: 65,
+  gender: 'M'
+}, {
+  name: 'Donald Trump',
+  age: 1,
+  gender: 'M'
+}, {
+  name: 'Theresa May',
+  age: 75,
+  gender: 'F'
+}, {
+  name: 'Jair Bolsonaro',
+  age: 67,
+  gender: 'M'
+}, {
+  name: 'Angela Merkel',
+  age: 600,
+  gender: 'F'
+}]
+
+function get (array) {
+  return array
+    .filter(({ gender }) => gender === 'F')
+    .sort(({ age: a }, { age: b }) => (
+      a < b ? 1 : a > b ? -1 : 0
+    ))
+    [0]
+}
+
+console.log(get(example))
