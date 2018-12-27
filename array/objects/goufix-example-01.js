77ec847ba7c264baef02c540848ee360472e2f51
Luiz Felipe <23662020+lffg@users.noreply.github.com>
2018-12-27 15:47:35 -0200

    add code

---
+++ array/objects/goufix-example-01.js
@@ -0,0 +1,22 @@
+const commands = [{
+  name: 'kick',
+  description: 'Kicka um usuário do quarto.'
+}, {
+  name: 'kickall',
+  description: 'Kicka todos os usuários do quarto.'
+}, {
+  name: 'pickall',
+  description: 'Recolhe todos os mobis do quarto.'
+}, {
+  name: 'jump',
+  description: 'Faz o personagem pular.'
+}, {
+  name: 'iddqd',
+  description: 'Espelha-girando a tela em 180 graus.'
+}]
+
+console.log(
+  commands  
+    .map(({ name, description }) => `:${name} --> ${description}`)
+    .join('\n')
+)
