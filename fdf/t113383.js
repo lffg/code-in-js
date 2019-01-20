9727df3304931eb57a36176d12dbd8811bc3e7a4
Luiz Felipe <23662020+lffg@users.noreply.github.com>
2019-01-20 10:02:36 -0200

    fix links

--- fdf/t113383.js
+++ fdf/t113383.js
@@ -2,7 +2,7 @@
  * Creates a post badge for each post.
  * (Only works in phpBB3 and ModernBB).
  *
- * @author Luiz Felipe F. <https://lffg.github.com>
+ * @author Luiz Felipe F. <https://lffg.github.io>
  * @see https://ajuda.forumeiros.com/t113383-
  * @see https://github.com/lffg/code-in-js/blob/master/fdf/t113383.js
  * @license MIT
