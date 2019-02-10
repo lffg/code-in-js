232763c678c8f0a427b84ac69899d7fb3f69c9ca
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2019-02-10 21:29:21 -0200

    Create calc-age-from-date.js

---
+++ utils/calc-age-from-date.js
@@ -0,0 +1,16 @@
+/**
+ * @see https://pt.stackoverflow.com/questions/5160/como-calcular-a-idade-de-uma-pessoa-com-js-a-partir-da-data-de-nascimento#answer-5162
+ */
+
+function getAge(birth) {
+  const current = new Date()
+  let diff = current.getFullYear() - birth.getFullYear()
+
+  if (
+    new Date(current.getFullYear(), current.getMonth(), current.getDate()) <
+    new Date(current.getFullYear(), birth.getMonth(), birth.getDate())
+  )
+    diff--
+
+  return diff
+}
