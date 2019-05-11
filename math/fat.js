285a3d929672354b8b1b492035a23b20331f2999
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2019-05-10 21:34:40 -0300

    Create fat.js

---
+++ math/fat.js
@@ -0,0 +1,27 @@
+/*global BigInt*/
+
+function fat(n, pretty = false, locale) {
+  if (typeof n !== 'number' || n >= Number.MAX_SAFE_INTEGER) {
+    throw new RangeError('Invalid number')
+  }
+
+  if (n <= 1) {
+    return 1n
+  }
+
+  let currentTotal = 1n
+  let currentNumber = BigInt(n)
+
+  while (currentNumber !== 1n) {
+    currentTotal *= currentNumber
+    currentNumber = currentNumber - 1n
+  }
+
+  if (pretty && typeof BigInt.toLocaleString === 'function') {
+    return currentTotal.toLocaleString(locale)
+  }
+
+  return currentTotal
+}
+
+console.log(fat(10))
