5fc1475dd21562c4ebd70f7923deb7d08ac79bdb
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2019-05-19 22:16:13 -0300

    ihuu

---
+++ examples/router.js
@@ -0,0 +1,50 @@
+class Router {
+  _list = []
+  _groupName = null
+
+  getList() {
+    return this._list
+  }
+
+  set(method, path) {
+    this._list.push(
+      [
+        `${typeof this._groupName === 'string' ? `(${this._groupName})` : ''}`,
+        `[${method}]`,
+        path
+      ]
+        .filter(Boolean)
+        .join(' ')
+    )
+  }
+
+  group(groupName, cb) {
+    const oldName = this._groupName
+
+    if (typeof oldName === 'string') {
+      this._groupName = `${oldName} -> ${groupName}`
+    } else {
+      this._groupName = groupName
+    }
+
+    cb()
+
+    this._groupName = oldName
+  }
+}
+
+const Route = new Router()
+
+Route.set('GET', '/')
+
+Route.group('User Routes', () => {
+  Route.set('GET', '/users')
+  Route.set('PUT', '/update-users')
+
+  Route.group('Profile Routes', () => {
+    Route.set('GET', '/users/:id')
+    Route.set('PUT', '/update-users/:id')
+  })
+})
+
+Route.set('GET', '/final')
