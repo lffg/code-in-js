d6ae2b959d9f8222a8879aec5fae35d36c197d9f
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2019-09-20 22:14:56 -0300

    Create list-html-pages.js

---
+++ fdf/list-html-pages.js
@@ -0,0 +1,49 @@
+/**
+ * List all custom HTML pages of a Forumactif Forum.
+ * This script may not work. It is currently unstable.
+ *
+ * @author Luiz Felipe Gonçalves <https://luizfelipe.dev>
+ * @license MIT
+ */
+
+/* eslint no-sequences: 0 */
+/* eslint no-unused-vars: 2 */
+/* eslint no-restricted-globals: 0 */
+
+const pageUrl = `${location.protocol}//${location.host}`.replace(/\/*$/, '');
+
+async function getHTML(uri) {
+  const response = await fetch(uri, { redirect: 'manual' });
+  return response.text();
+}
+
+function getTitle(pageContents, uri) {
+  if (!pageContents) {
+    return `${uri}: ERROR!`;
+  }
+
+  const parser = new DOMParser();
+  const document = parser.parseFromString(pageContents, 'text/html');
+
+  const title = (document.querySelector('title') || {}).textContent;
+  return `${uri}: ${title || 'No title.'}`;
+}
+
+async function fetchPages(from = 0, add = 1) {
+  const count = from + add;
+  console.warn(`Loading pages ${from} ~ ${count} ...`);
+
+  const promises = Array.from({ length: count })
+    .map((_, index) => `${pageUrl}/h${index + 1}-`)
+    .map(async (link) => [await getHTML(link), link]);
+
+  const done = await Promise.allSettled(promises);
+
+  return done.map(({ status, value: [html, uri] }) =>
+    getTitle(status === 'fulfilled' ? html : null, uri)
+  );
+}
+
+fetchPages(1, 50).then(
+  (array) => (console.log('Done:'), console.log(array.join('\n')))
+);
